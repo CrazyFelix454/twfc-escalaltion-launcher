a47b6398f41b6e5ce62c20c74b546a68bbfd9d1d
Ryan Tredway <102388383+CrazyFelix454@users.noreply.github.com>
2023-06-27 02:04:28 -0500

    v0.2
    Changes:
    - Removed "Offline" from the window title & banner text
    + Added 3 more maps to the map list (may require DLC)
    + Altered some text in drive letter example
    + Changed some function names (made their names make sense)
    + Moved FILE_PATH check for the word "EXAMPLE" to the end of GET_FILE_PATH to avoid any possible issues
    + Added a few new functions to check if the game is running or not (look for "PROCESS" functions)
    + Added a check to see if the user is done playing or would like to play another map
    + Other small cosmetic changes to make text easier to read

--- twfc-escalation-launcher/src/twfc-escalation-launcher.bat
+++ twfc-escalation-launcher/src/twfc-escalation-launcher.bat
@@ -1,9 +1,9 @@
 @echo off
-title WFC - Offline Escalation Launcher
+title WFC - Escalation Launcher
 color 07
 
 @REM Please do not alter VERSION or AUTHOR
-@set VERSION=0.1
+@set VERSION=0.2
 @set AUTHOR=Grumpee (CrazyFelix454 on GitHub)
 
 
@@ -20,7 +20,7 @@ color 07
     echo    `8b8' `8d8'  YP       `Y88P'
     echo ---------------------------------
     echo.
-    echo    Offline Escalation Launcher
+    echo        Escalation Launcher
     echo.
     echo.
     TIMEOUT 1 >nul
@@ -30,43 +30,54 @@ color 07
     echo.
     TIMEOUT 1 >nul
 
-:SAME_FOLDER_CHECK
+:FOLDER_CHECK
     echo.
-    @set /P SAME_FOLDER_CHECK_ANSWER=Did you place this batch file in the same folder as TWFC.exe? - y/n:
+    @set /P FOLDER_CHECK_ANSWER=Did you place this batch file in the same folder as TWFC.exe? - y/n:
     echo.
 
-    if %SAME_FOLDER_CHECK_ANSWER%==y (
+    if %FOLDER_CHECK_ANSWER%==y (
         cls
         goto :SELECT_MAP
-    ) else if %SAME_FOLDER_CHECK_ANSWER%==n (
+    ) else if %FOLDER_CHECK_ANSWER%==n (
         cls 
-        goto :GETTING_DRIVE
+        goto :GET_DRIVE
     ) else (
+        cls
+        echo.
         echo ERROR :: Unknown response, please try again
-        goto :SAME_FOLDER_CHECK
+        echo.
+        goto :FOLDER_CHECK
     )
 
-:GETTING_DRIVE
+:GET_DRIVE
     echo.
     echo Type the drive letter that Transformers War For Cybertron is installed on.
-    echo Example: C, D, E, etc. (This is case sensitive)
+    echo Example: C, D, E, etc. (This is does not need to be capitalized)
     echo.
-    @set /P DRIVE_LETTER=
+    @set /P DRIVE_LETTER=Drive:
     %DRIVE_LETTER%:
+    echo.
     echo Drive changed
 
-:GETTING_FILE_PATH
+:GET_FILE_PATH
     echo.
     echo Type the full file path to Transformers War For Cybertron on your PC.
     echo Example: Steam\steamapps\common\Transformers War for Cybertron\Binaries
     echo.
     @set /P FILE_PATH=Your file path:
 
-:CHECK_FILE_PATH
     if %FILE_PATH%==EXAMPLE (
         @set FILE_PATH=Steam\steamapps\common\Transformers War for Cybertron\Binaries
+    ) else if %FILE_PATH%==example (
+        @set FILE_PATH=Steam\steamapps\common\Transformers War for Cybertron\Binaries
+    ) else (
+        cls
+        echo.
+        echo Oops... something went wrong, please try again
+        goto :GET_FILE_PATH
     )
 
+:CHECK_FILE_PATH
     echo.
     echo %FILE_PATH%
     echo.
@@ -74,7 +85,7 @@ color 07
 
     if %FILE_PATH_ANSWER%==n (
         cls
-        goto :GETTING_FILE_PATH
+        goto :GET_FILE_PATH
     ) else if %FILE_PATH_ANSWER%==y (
         cls
         cd %FILE_PATH%
@@ -88,16 +99,30 @@ color 07
 :SELECT_MAP
     @set Broke=MP_ESC_BrokenHope_BASE_m
     @set Rem=MP_ESC_Remnant_BASE_m
+    @set Dis=MP_ESC_District_BASE_m
+    @REM Do not un-comment Pulse - causes instant game crash
+    @REM @set Pul=MP_ESC_Pulse_BASE_m
+    @set Sta=MP_ESC_Static_BASE_m
+    @set Tra=MP_ESC_Tranquillity_BASE_m
 
     echo.
     echo.
     echo --------------MAPS--------------
     echo            Broken Hope
     echo              Remnant
+    echo             District      *
+    @REM echo              Pulse        Mod Required
+    echo              Static       *
+    echo            Tranquillity   *
     echo --------------------------------
     echo.
     echo Please select a map from the list above.
-    echo -- Type 1 for Broken Hope or 2 for Remnant
+    echo -- Type 1 for Broken Hope or 2 for Remnant and so on..
+    echo.
+    echo.
+    echo Note: If a map has a '*' next to it, that map is playable but has issues..
+    echo (i.e.: broken textures, effects, or audio)
+    echo.
     echo.
     @set /P MAP_NAME=Map:
 
@@ -107,16 +132,68 @@ color 07
     ) else if %MAP_NAME%==2 (
         @set MAP=%Rem%
         goto :RUN_GAME
+    ) else if %MAP_NAME%==3 (
+        @set MAP=%Dis%
+        goto :RUN_GAME
+    ) 
+    @REM else if %MAP_NAME%==4 (
+    @REM     @set MAP=%Pul%
+    @REM     goto :RUN_GAME
+    @REM ) 
+    else if %MAP_NAME%==4 (
+        @set MAP=%Sta%
+        goto :RUN_GAME
+    ) else if %MAP_NAME%==5 (
+        @set MAP=%Tra%
+        goto :RUN_GAME
     ) else (
+        cls
         echo.
         echo ERROR :: Unknown response, please try again
         goto :SELECT_MAP
     )
 
 :RUN_GAME
-    echo Game is starting, please wait...
+    echo Launching game...
     twfc %MAP%?Game=TransContent.TnSurvivalGame?GameModeTag=SV?GameTeamStatus=2
-    goto :END
+    TIMEOUT 30 >nul
+    goto :PROCESS_CHECK
+
+:PROCESS_CHECK
+    SETLOCAL EnableExtensions
+    set EXE=TWFC.exe
+    for /F %%x in (
+        'tasklist /NH /FI "IMAGENAME eq %EXE%"'
+    ) do if %%x == %EXE% goto :PROCESS_FOUND
+    goto :PROCESS_NOT_FOUND
+
+:PROCESS_FOUND
+    cls
+    echo.
+    echo %EXE% is running...
+    TIMEOUT 5 >nul
+    goto :PROCESS_CHECK
+:PROCESS_NOT_FOUND
+    echo.
+    echo %EXE% is not running...
+    TIMEOUT 2 >nul
+    goto :END_CHECK
+
+:END_CHECK
+    echo.
+    @set /P END_CHECK_RESPONSE=Are you finished? - y/n:
+    
+    if %END_CHECK_RESPONSE%==n (
+        cls
+        goto :SELECT_MAP
+    ) else if %END_CHECK_RESPONSE%==y (
+        goto :END
+    ) else (
+        cls
+        echo.
+        echo ERROR :: Unknown response, please try again
+        goto :END_CHECK
+    )
 
 :END
     cls
